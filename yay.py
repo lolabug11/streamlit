042c3688377179240675a2e7d15f607bf44c0cc1
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-07-15 23:04:35 +0000

    :))

--- yay.py
+++ yay.py
@@ -1,13 +1,21 @@
 import streamlit as st
 from math import *
+import pandas as pd
+import numpy as np
 st.title("Quadratic solver")
-a = st.slider('A', 0.0,10000.0)
-b = st.slider('B', 0.0,10000.0)
-c = st.slider('C', 0.0,10000.0)
-b_squared_minus_four_a_c = b** - 4 * a * c
-positive = -b + sqrt(b_squared_minus_four_a_c)/2*a
-negative = -b - sqrt(b_squared_minus_four_a_c)/2*a
-if positive == negative:
-    st.write(f'The Awnser is {positive}')
+a = float(st.text_input("A", 1.0,help="Enter the a value"))
+b = float(st.text_input("B", 0.0))
+c = float(st.text_input("C", 0.0))
+b_squared_minus_four_a_c = b**2 - 4 * a * c
+if b_squared_minus_four_a_c > 0:
+    positive = -b + sqrt(b_squared_minus_four_a_c)/2*a
+    negative = -b - sqrt(b_squared_minus_four_a_c)/2*a
+    vertex = (positive+negative)/2
+    if positive == negative:
+        st.write(f'The Solution is {positive}')
+    else:
+        data = {"plus":[positive], "minus":[negative], "vertex":[vertex]}
+        dataframe = pd.DataFrame(data)
+        st.table(dataframe,width="content")
 else:
-    st.write(f'The + of the +- is {positive}\nThe - of the -+ is {negative}')
+    st.write("The vertexes would be complex numbers!")
